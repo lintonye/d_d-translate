641efbdaeeff4e775aa8d1301d805c39e417ca5c
Linton Ye <linton@jimulabs.com>
2021-12-07 22:26:36 -0800

    Initial commit

---
+++ backend/pages/api/translate.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  id: string;
+  translatedText: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ translatedText: "John Doe", id: "123" });
+}
---
+++ chrome-extension/background.js
@@ -0,0 +1,49 @@
+function translatePage() {
+  // let menubar = document.createElement("div");
+  // menubar.appendChild(document.createTextNode("Menu"));
+  // document.body.insertAdjacentElement("afterbegin", menubar);
+
+  function walkDOM(node, callback) {
+    for (let c of node.childNodes) {
+      callback(c, node);
+      walkDOM(c, callback);
+    }
+  }
+
+  async function translate(id, lang, text) {
+    const response = await fetch(
+      "https://d_d-tranlate.vercel.com/api/translate",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, lang, text }),
+      }
+    );
+    const data = await response.json();
+    return data.translatedText;
+  }
+
+  walkDOM(document.body, async (node, parent) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      let text = node.textContent;
+      let hashBuffer = await crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode(text)
+      );
+      let id = Array.from(new Uint8Array(hashBuffer))
+        .map((b) => b.toString(16))
+        .join("");
+      node.textContent = await translate(id, "zh-HANS", text);
+      if (!parent.id) parent.id = id;
+    }
+  });
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: translatePage,
+  });
+});
