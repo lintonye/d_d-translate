a93940c327bc42e578a1b8bf36cd4f39a8f91e0c
Linton Ye <linton@jimulabs.com>
2021-12-07 23:07:13 -0800

    Allow CORS

--- backend/pages/api/translate.ts
+++ backend/pages/api/translate.ts
@@ -6,10 +6,30 @@ type Data = {
   translatedText: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+const allowCors =
+  (fn: (req: NextApiRequest, res: NextApiResponse) => void) =>
+  async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({ translatedText: "John Doe", id: "123" });
 }
+
+export default allowCors(handler);
